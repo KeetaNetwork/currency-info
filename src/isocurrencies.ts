30b3060df39b699ca028539f22335c5a4ccb64c3
Ezra Ripps <eripps@keeta.com>
2023-01-12 14:39:42 -0800

    iso codes

---
+++ src/isocurrencies.ts
@@ -0,0 +1,1006 @@
+const ISOCurrencies = [
+    {
+        code: "AFN",
+        name: "Afghani",
+        precision: "2",
+        isoNumber: "971"
+    },
+    {
+        code: "EUR",
+        name: "Euro",
+        precision: "2",
+        isoNumber: "978"
+    },
+    {
+        code: "ALL",
+        name: "Lek",
+        precision: "2",
+        isoNumber: "8"
+    },
+    {
+        code: "DZD",
+        name: "Algerian Dinar",
+        precision: "2",
+        isoNumber: "12"
+    },
+    {
+        code: "USD",
+        name: "US Dollar",
+        precision: "2",
+        isoNumber: "840"
+    },
+    {
+        code: "AOA",
+        name: "Kwanza",
+        precision: "2",
+        isoNumber: "973"
+    },
+    {
+        code: "XCD",
+        name: "East Caribbean Dollar",
+        precision: "2",
+        isoNumber: "951"
+    },
+    {
+        code: "ARS",
+        name: "Argentine Peso",
+        precision: "2",
+        isoNumber: "32"
+    },
+    {
+        code: "AMD",
+        name: "Armenian Dram",
+        precision: "2",
+        isoNumber: "51"
+    },
+    {
+        code: "AWG",
+        name: "Aruban Florin",
+        precision: "2",
+        isoNumber: "533"
+    },
+    {
+        code: "AUD",
+        name: "Australian Dollar",
+        precision: "2",
+        isoNumber: "36"
+    },
+    {
+        code: "AZN",
+        name: "Azerbaijan Manat",
+        precision: "2",
+        isoNumber: "944"
+    },
+    {
+        code: "BSD",
+        name: "Bahamian Dollar",
+        precision: "2",
+        isoNumber: "44"
+    },
+    {
+        code: "BHD",
+        name: "Bahraini Dinar",
+        precision: "3",
+        isoNumber: "48"
+    },
+    {
+        code: "BDT",
+        name: "Taka",
+        precision: "2",
+        isoNumber: "50"
+    },
+    {
+        code: "BBD",
+        name: "Barbados Dollar",
+        precision: "2",
+        isoNumber: "52"
+    },
+    {
+        code: "BYN",
+        name: "Belarusian Ruble",
+        precision: "2",
+        isoNumber: "933"
+    },
+    {
+        code: "BZD",
+        name: "Belize Dollar",
+        precision: "2",
+        isoNumber: "84"
+    },
+    {
+        code: "XOF",
+        name: "CFA Franc BCEAO",
+        precision: "0",
+        isoNumber: "952"
+    },
+    {
+        code: "BMD",
+        name: "Bermudian Dollar",
+        precision: "2",
+        isoNumber: "60"
+    },
+    {
+        code: "INR",
+        name: "Indian Rupee",
+        precision: "2",
+        isoNumber: "356"
+    },
+    {
+        code: "BTN",
+        name: "Ngultrum",
+        precision: "2",
+        isoNumber: "64"
+    },
+    {
+        code: "BOB",
+        name: "Boliviano",
+        precision: "2",
+        isoNumber: "68"
+    },
+    {
+        code: "BOV",
+        name: "Mvdol",
+        precision: "2",
+        isoNumber: "984"
+    },
+    {
+        code: "BAM",
+        name: "Convertible Mark",
+        precision: "2",
+        isoNumber: "977"
+    },
+    {
+        code: "BWP",
+        name: "Pula",
+        precision: "2",
+        isoNumber: "72"
+    },
+    {
+        code: "NOK",
+        name: "Norwegian Krone",
+        precision: "2",
+        isoNumber: "578"
+    },
+    {
+        code: "BRL",
+        name: "Brazilian Real",
+        precision: "2",
+        isoNumber: "986"
+    },
+    {
+        code: "BND",
+        name: "Brunei Dollar",
+        precision: "2",
+        isoNumber: "96"
+    },
+    {
+        code: "BGN",
+        name: "Bulgarian Lev",
+        precision: "2",
+        isoNumber: "975"
+    },
+    {
+        code: "BIF",
+        name: "Burundi Franc",
+        precision: "0",
+        isoNumber: "108"
+    },
+    {
+        code: "CVE",
+        name: "Cabo Verde Escudo",
+        precision: "2",
+        isoNumber: "132"
+    },
+    {
+        code: "KHR",
+        name: "Riel",
+        precision: "2",
+        isoNumber: "116"
+    },
+    {
+        code: "XAF",
+        name: "CFA Franc BEAC",
+        precision: "0",
+        isoNumber: "950"
+    },
+    {
+        code: "CAD",
+        name: "Canadian Dollar",
+        precision: "2",
+        isoNumber: "124"
+    },
+    {
+        code: "KYD",
+        name: "Cayman Islands Dollar",
+        precision: "2",
+        isoNumber: "136"
+    },
+    {
+        code: "CLP",
+        name: "Chilean Peso",
+        precision: "0",
+        isoNumber: "152"
+    },
+    {
+        code: "CLF",
+        name: "Unidad de Fomento",
+        precision: "4",
+        isoNumber: "990"
+    },
+    {
+        code: "CNY",
+        name: "Yuan Renminbi",
+        precision: "2",
+        isoNumber: "156"
+    },
+    {
+        code: "COP",
+        name: "Colombian Peso",
+        precision: "2",
+        isoNumber: "170"
+    },
+    {
+        code: "COU",
+        name: "Unidad de Valor Real",
+        precision: "2",
+        isoNumber: "970"
+    },
+    {
+        code: "KMF",
+        name: "Comorian Franc",
+        precision: "0",
+        isoNumber: "174"
+    },
+    {
+        code: "CDF",
+        name: "Congolese Franc",
+        precision: "2",
+        isoNumber: "976"
+    },
+    {
+        code: "NZD",
+        name: "New Zealand Dollar",
+        precision: "2",
+        isoNumber: "554"
+    },
+    {
+        code: "CRC",
+        name: "Costa Rican Colon",
+        precision: "2",
+        isoNumber: "188"
+    },
+    {
+        code: "CUP",
+        name: "Cuban Peso",
+        precision: "2",
+        isoNumber: "192"
+    },
+    {
+        code: "CUC",
+        name: "Peso Convertible",
+        precision: "2",
+        isoNumber: "931"
+    },
+    {
+        code: "ANG",
+        name: "Netherlands Antillean Guilder",
+        precision: "2",
+        isoNumber: "532"
+    },
+    {
+        code: "CZK",
+        name: "Czech Koruna",
+        precision: "2",
+        isoNumber: "203"
+    },
+    {
+        code: "DKK",
+        name: "Danish Krone",
+        precision: "2",
+        isoNumber: "208"
+    },
+    {
+        code: "DJF",
+        name: "Djibouti Franc",
+        precision: "0",
+        isoNumber: "262"
+    },
+    {
+        code: "DOP",
+        name: "Dominican Peso",
+        precision: "2",
+        isoNumber: "214"
+    },
+    {
+        code: "EGP",
+        name: "Egyptian Pound",
+        precision: "2",
+        isoNumber: "818"
+    },
+    {
+        code: "SVC",
+        name: "El Salvador Colon",
+        precision: "2",
+        isoNumber: "222"
+    },
+    {
+        code: "ERN",
+        name: "Nakfa",
+        precision: "2",
+        isoNumber: "232"
+    },
+    {
+        code: "SZL",
+        name: "Lilangeni",
+        precision: "2",
+        isoNumber: "748"
+    },
+    {
+        code: "ETB",
+        name: "Ethiopian Birr",
+        precision: "2",
+        isoNumber: "230"
+    },
+    {
+        code: "FKP",
+        name: "Falkland Islands Pound",
+        precision: "2",
+        isoNumber: "238"
+    },
+    {
+        code: "FJD",
+        name: "Fiji Dollar",
+        precision: "2",
+        isoNumber: "242"
+    },
+    {
+        code: "XPF",
+        name: "CFP Franc",
+        precision: "0",
+        isoNumber: "953"
+    },
+    {
+        code: "GMD",
+        name: "Dalasi",
+        precision: "2",
+        isoNumber: "270"
+    },
+    {
+        code: "GEL",
+        name: "Lari",
+        precision: "2",
+        isoNumber: "981"
+    },
+    {
+        code: "GHS",
+        name: "Ghana Cedi",
+        precision: "2",
+        isoNumber: "936"
+    },
+    {
+        code: "GIP",
+        name: "Gibraltar Pound",
+        precision: "2",
+        isoNumber: "292"
+    },
+    {
+        code: "GTQ",
+        name: "Quetzal",
+        precision: "2",
+        isoNumber: "320"
+    },
+    {
+        code: "GBP",
+        name: "Pound Sterling",
+        precision: "2",
+        isoNumber: "826"
+    },
+    {
+        code: "GNF",
+        name: "Guinean Franc",
+        precision: "0",
+        isoNumber: "324"
+    },
+    {
+        code: "GYD",
+        name: "Guyana Dollar",
+        precision: "2",
+        isoNumber: "328"
+    },
+    {
+        code: "HTG",
+        name: "Gourde",
+        precision: "2",
+        isoNumber: "332"
+    },
+    {
+        code: "HNL",
+        name: "Lempira",
+        precision: "2",
+        isoNumber: "340"
+    },
+    {
+        code: "HKD",
+        name: "Hong Kong Dollar",
+        precision: "2",
+        isoNumber: "344"
+    },
+    {
+        code: "HUF",
+        name: "Forint",
+        precision: "2",
+        isoNumber: "348"
+    },
+    {
+        code: "ISK",
+        name: "Iceland Krona",
+        precision: "0",
+        isoNumber: "352"
+    },
+    {
+        code: "IDR",
+        name: "Rupiah",
+        precision: "2",
+        isoNumber: "360"
+    },
+    {
+        code: "IRR",
+        name: "Iranian Rial",
+        precision: "2",
+        isoNumber: "364"
+    },
+    {
+        code: "IQD",
+        name: "Iraqi Dinar",
+        precision: "3",
+        isoNumber: "368"
+    },
+    {
+        code: "ILS",
+        name: "New Israeli Sheqel",
+        precision: "2",
+        isoNumber: "376"
+    },
+    {
+        code: "JMD",
+        name: "Jamaican Dollar",
+        precision: "2",
+        isoNumber: "388"
+    },
+    {
+        code: "JPY",
+        name: "Yen",
+        precision: "0",
+        isoNumber: "392"
+    },
+    {
+        code: "JOD",
+        name: "Jordanian Dinar",
+        precision: "3",
+        isoNumber: "400"
+    },
+    {
+        code: "KZT",
+        name: "Tenge",
+        precision: "2",
+        isoNumber: "398"
+    },
+    {
+        code: "KES",
+        name: "Kenyan Shilling",
+        precision: "2",
+        isoNumber: "404"
+    },
+    {
+        code: "KPW",
+        name: "North Korean Won",
+        precision: "2",
+        isoNumber: "408"
+    },
+    {
+        code: "KRW",
+        name: "Won",
+        precision: "0",
+        isoNumber: "410"
+    },
+    {
+        code: "KWD",
+        name: "Kuwaiti Dinar",
+        precision: "3",
+        isoNumber: "414"
+    },
+    {
+        code: "KGS",
+        name: "Som",
+        precision: "2",
+        isoNumber: "417"
+    },
+    {
+        code: "LAK",
+        name: "Lao Kip",
+        precision: "2",
+        isoNumber: "418"
+    },
+    {
+        code: "LBP",
+        name: "Lebanese Pound",
+        precision: "2",
+        isoNumber: "422"
+    },
+    {
+        code: "LSL",
+        name: "Loti",
+        precision: "2",
+        isoNumber: "426"
+    },
+    {
+        code: "ZAR",
+        name: "Rand",
+        precision: "2",
+        isoNumber: "710"
+    },
+    {
+        code: "LRD",
+        name: "Liberian Dollar",
+        precision: "2",
+        isoNumber: "430"
+    },
+    {
+        code: "LYD",
+        name: "Libyan Dinar",
+        precision: "3",
+        isoNumber: "434"
+    },
+    {
+        code: "CHF",
+        name: "Swiss Franc",
+        precision: "2",
+        isoNumber: "756"
+    },
+    {
+        code: "MOP",
+        name: "Pataca",
+        precision: "2",
+        isoNumber: "446"
+    },
+    {
+        code: "MKD",
+        name: "Denar",
+        precision: "2",
+        isoNumber: "807"
+    },
+    {
+        code: "MGA",
+        name: "Malagasy Ariary",
+        precision: "2",
+        isoNumber: "969"
+    },
+    {
+        code: "MWK",
+        name: "Malawi Kwacha",
+        precision: "2",
+        isoNumber: "454"
+    },
+    {
+        code: "MYR",
+        name: "Malaysian Ringgit",
+        precision: "2",
+        isoNumber: "458"
+    },
+    {
+        code: "MVR",
+        name: "Rufiyaa",
+        precision: "2",
+        isoNumber: "462"
+    },
+    {
+        code: "MRU",
+        name: "Ouguiya",
+        precision: "2",
+        isoNumber: "929"
+    },
+    {
+        code: "MUR",
+        name: "Mauritius Rupee",
+        precision: "2",
+        isoNumber: "480"
+    },
+    {
+        code: "MXN",
+        name: "Mexican Peso",
+        precision: "2",
+        isoNumber: "484"
+    },
+    {
+        code: "MXV",
+        name: "Mexican Unidad de Inversion (UDI)",
+        precision: "2",
+        isoNumber: "979"
+    },
+    {
+        code: "MDL",
+        name: "Moldovan Leu",
+        precision: "2",
+        isoNumber: "498"
+    },
+    {
+        code: "MNT",
+        name: "Tugrik",
+        precision: "2",
+        isoNumber: "496"
+    },
+    {
+        code: "MAD",
+        name: "Moroccan Dirham",
+        precision: "2",
+        isoNumber: "504"
+    },
+    {
+        code: "MZN",
+        name: "Mozambique Metical",
+        precision: "2",
+        isoNumber: "943"
+    },
+    {
+        code: "MMK",
+        name: "Kyat",
+        precision: "2",
+        isoNumber: "104"
+    },
+    {
+        code: "NAD",
+        name: "Namibia Dollar",
+        precision: "2",
+        isoNumber: "516"
+    },
+    {
+        code: "NPR",
+        name: "Nepalese Rupee",
+        precision: "2",
+        isoNumber: "524"
+    },
+    {
+        code: "NIO",
+        name: "Cordoba Oro",
+        precision: "2",
+        isoNumber: "558"
+    },
+    {
+        code: "NGN",
+        name: "Naira",
+        precision: "2",
+        isoNumber: "566"
+    },
+    {
+        code: "OMR",
+        name: "Rial Omani",
+        precision: "3",
+        isoNumber: "512"
+    },
+    {
+        code: "PKR",
+        name: "Pakistan Rupee",
+        precision: "2",
+        isoNumber: "586"
+    },
+    {
+        code: "PAB",
+        name: "Balboa",
+        precision: "2",
+        isoNumber: "590"
+    },
+    {
+        code: "PGK",
+        name: "Kina",
+        precision: "2",
+        isoNumber: "598"
+    },
+    {
+        code: "PYG",
+        name: "Guarani",
+        precision: "0",
+        isoNumber: "600"
+    },
+    {
+        code: "PEN",
+        name: "Sol",
+        precision: "2",
+        isoNumber: "604"
+    },
+    {
+        code: "PHP",
+        name: "Philippine Peso",
+        precision: "2",
+        isoNumber: "608"
+    },
+    {
+        code: "PLN",
+        name: "Zloty",
+        precision: "2",
+        isoNumber: "985"
+    },
+    {
+        code: "QAR",
+        name: "Qatari Rial",
+        precision: "2",
+        isoNumber: "634"
+    },
+    {
+        code: "RON",
+        name: "Romanian Leu",
+        precision: "2",
+        isoNumber: "946"
+    },
+    {
+        code: "RUB",
+        name: "Russian Ruble",
+        precision: "2",
+        isoNumber: "643"
+    },
+    {
+        code: "RWF",
+        name: "Rwanda Franc",
+        precision: "0",
+        isoNumber: "646"
+    },
+    {
+        code: "SHP",
+        name: "Saint Helena Pound",
+        precision: "2",
+        isoNumber: "654"
+    },
+    {
+        code: "WST",
+        name: "Tala",
+        precision: "2",
+        isoNumber: "882"
+    },
+    {
+        code: "STN",
+        name: "Dobra",
+        precision: "2",
+        isoNumber: "930"
+    },
+    {
+        code: "SAR",
+        name: "Saudi Riyal",
+        precision: "2",
+        isoNumber: "682"
+    },
+    {
+        code: "RSD",
+        name: "Serbian Dinar",
+        precision: "2",
+        isoNumber: "941"
+    },
+    {
+        code: "SCR",
+        name: "Seychelles Rupee",
+        precision: "2",
+        isoNumber: "690"
+    },
+    {
+        code: "SLL",
+        name: "Leone",
+        precision: "2",
+        isoNumber: "694"
+    },
+    {
+        code: "SLE",
+        name: "Leone",
+        precision: "2",
+        isoNumber: "925"
+    },
+    {
+        code: "SGD",
+        name: "Singapore Dollar",
+        precision: "2",
+        isoNumber: "702"
+    },
+    {
+        code: "SBD",
+        name: "Solomon Islands Dollar",
+        precision: "2",
+        isoNumber: "90"
+    },
+    {
+        code: "SOS",
+        name: "Somali Shilling",
+        precision: "2",
+        isoNumber: "706"
+    },
+    {
+        code: "SSP",
+        name: "South Sudanese Pound",
+        precision: "2",
+        isoNumber: "728"
+    },
+    {
+        code: "LKR",
+        name: "Sri Lanka Rupee",
+        precision: "2",
+        isoNumber: "144"
+    },
+    {
+        code: "SDG",
+        name: "Sudanese Pound",
+        precision: "2",
+        isoNumber: "938"
+    },
+    {
+        code: "SRD",
+        name: "Surinam Dollar",
+        precision: "2",
+        isoNumber: "968"
+    },
+    {
+        code: "SEK",
+        name: "Swedish Krona",
+        precision: "2",
+        isoNumber: "752"
+    },
+    {
+        code: "CHE",
+        name: "WIR Euro",
+        precision: "2",
+        isoNumber: "947"
+    },
+    {
+        code: "CHW",
+        name: "WIR Franc",
+        precision: "2",
+        isoNumber: "948"
+    },
+    {
+        code: "SYP",
+        name: "Syrian Pound",
+        precision: "2",
+        isoNumber: "760"
+    },
+    {
+        code: "TWD",
+        name: "New Taiwan Dollar",
+        precision: "2",
+        isoNumber: "901"
+    },
+    {
+        code: "TJS",
+        name: "Somoni",
+        precision: "2",
+        isoNumber: "972"
+    },
+    {
+        code: "TZS",
+        name: "Tanzanian Shilling",
+        precision: "2",
+        isoNumber: "834"
+    },
+    {
+        code: "THB",
+        name: "Baht",
+        precision: "2",
+        isoNumber: "764"
+    },
+    {
+        code: "TOP",
+        name: "Pa’anga",
+        precision: "2",
+        isoNumber: "776"
+    },
+    {
+        code: "TTD",
+        name: "Trinidad and Tobago Dollar",
+        precision: "2",
+        isoNumber: "780"
+    },
+    {
+        code: "TND",
+        name: "Tunisian Dinar",
+        precision: "3",
+        isoNumber: "788"
+    },
+    {
+        code: "TRY",
+        name: "Turkish Lira",
+        precision: "2",
+        isoNumber: "949"
+    },
+    {
+        code: "TMT",
+        name: "Turkmenistan New Manat",
+        precision: "2",
+        isoNumber: "934"
+    },
+    {
+        code: "UGX",
+        name: "Uganda Shilling",
+        precision: "0",
+        isoNumber: "800"
+    },
+    {
+        code: "UAH",
+        name: "Hryvnia",
+        precision: "2",
+        isoNumber: "980"
+    },
+    {
+        code: "AED",
+        name: "UAE Dirham",
+        precision: "2",
+        isoNumber: "784"
+    },
+    {
+        code: "USN",
+        name: "US Dollar (Next day)",
+        precision: "2",
+        isoNumber: "997"
+    },
+    {
+        code: "UYU",
+        name: "Peso Uruguayo",
+        precision: "2",
+        isoNumber: "858"
+    },
+    {
+        code: "UYI",
+        name: "Uruguay Peso en Unidades Indexadas (UI)",
+        precision: "0",
+        isoNumber: "940"
+    },
+    {
+        code: "UYW",
+        name: "Unidad Previsional",
+        precision: "4",
+        isoNumber: "927"
+    },
+    {
+        code: "UZS",
+        name: "Uzbekistan Sum",
+        precision: "2",
+        isoNumber: "860"
+    },
+    {
+        code: "VUV",
+        name: "Vatu",
+        precision: "0",
+        isoNumber: "548"
+    },
+    {
+        code: "VES",
+        name: "Bolívar Soberano",
+        precision: "2",
+        isoNumber: "928"
+    },
+    {
+        code: "VED",
+        name: "Bolívar Soberano",
+        precision: "2",
+        isoNumber: "926"
+    },
+    {
+        code: "VND",
+        name: "Dong",
+        precision: "0",
+        isoNumber: "704"
+    },
+    {
+        code: "YER",
+        name: "Yemeni Rial",
+        precision: "2",
+        isoNumber: "886"
+    },
+    {
+        code: "ZMW",
+        name: "Zambian Kwacha",
+        precision: "2",
+        isoNumber: "967"
+    },
+    {
+        code: "ZWL",
+        name: "Zimbabwe Dollar",
+        precision: "2",
+        isoNumber: "932"
+    }
+] as const;
+
+export default ISOCurrencies;
