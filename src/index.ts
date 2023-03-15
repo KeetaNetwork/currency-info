9772d8e852513ab4e58ab677aa2fdd0869cb11ea
Tanveer Wahid <twahid@keeta.com>
2023-03-15 09:24:59 -0700

    And functions for currency numbers.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@ import { CurrencyCode, currencyNames } from './types.generated'
 import ISOCurrencies from './isocurrencies';
 
 export function deleteCurrencies(codes: CurrencyCode[]): void {
-    codes.forEach((code) => {
-        const index = currencyNames.indexOf(code);
-        if (index > -1) {
-            (currencyNames as unknown as string[]).splice(index, 1);
-        }
-    });
+	codes.forEach((code) => {
+		const index = currencyNames.indexOf(code);
+		if (index > -1) {
+			(currencyNames as unknown as string[]).splice(index, 1);
+		}
+	});
 }
 
 export function isCurrencyCode(code: any): code is CurrencyCode {
-    return currencyNames.includes(code);
+	return currencyNames.includes(code);
+}
+
+export function isCurrencyNumber(number: any): number is ISOCurrencyNumber {
+	return ISOCurrencies.some((currency) => currency.isoNumber === String(number));
 }
 
 export function assertCurrencyCode(code: any): CurrencyCode {
@@ -22,18 +26,44 @@ export function assertCurrencyCode(code: any): CurrencyCode {
 	return(code);
 }
 
+export function assertCurrencyNumber(number: any): ISOCurrencyNumber {
+	if (!isCurrencyNumber(number)) {
+		throw(new Error(`Invalid ISO number: ${number}`));
+	}
+
+	return(number);
+}
+
+export function currencyNumberToCurrencyCode(isoNumber: ISOCurrencyNumber): CurrencyCode {
+	const currency = getByISONumber(isoNumber);
+	if (!currency || !isCurrencyCode(currency.code)) {
+		throw(new Error(`Invalid currency number: ${isoNumber}`));
+	}
+
+	return currency.code;
+}
+
+export function currencyCodeToCurrencyNumber(code: CurrencyCode): ISOCurrencyNumber {
+	const currency = getByISOCode(code);
+	if (!currency) {
+		throw(new Error(`Invalid currency code: ${code}`));
+	}
+
+	return currency.isoNumber;
+}
+
 // Convert usd.svg -> USD, eur.svg -> EUR, etc
 function fileNameToCurrencyCode(fileName: string): CurrencyCode {
-    // Check if the currency code provided matches the CurrencyCode type
-    const withoutExtension = fileName.replace(/(\.svg)|(\.\/)/g, '');
-    const currencyCode = withoutExtension.toUpperCase();
+	// Check if the currency code provided matches the CurrencyCode type
+	const withoutExtension = fileName.replace(/(\.svg)|(\.\/)/g, '');
+	const currencyCode = withoutExtension.toUpperCase();
 
-    // We should throw an error if for some reason a file provided isnt a valid CurrencyCode
-    if (!(isCurrencyCode(currencyCode))) {
-        throw(new Error(`Invalid SVG name ${fileName}`));
-    }
+	// We should throw an error if for some reason a file provided is not a valid CurrencyCode
+	if (!(isCurrencyCode(currencyCode))) {
+		throw(new Error(`Invalid SVG name ${fileName}`));
+	}
 
-    return currencyCode;
+	return currencyCode;
 }
 
 type UnionKeyObject<U extends string, V> = { [K in U]?: V };
@@ -45,12 +75,12 @@ const tmpImages: ImageObject = {};
 const allSvgs = require.context('./svg', false, /\.svg$/);
 const allSvgFileNames = allSvgs.keys();
 
-for (const fileName of allSvgFileNames) {    
-    // Get currency code from file name
-    const currencyCode = fileNameToCurrencyCode(fileName);
+for (const fileName of allSvgFileNames) {
+	// Get currency code from file name
+	const currencyCode = fileNameToCurrencyCode(fileName);
 
-    // Get content of specific file name from key
-    tmpImages[currencyCode] = allSvgs(fileName);
+	// Get content of specific file name from key
+	tmpImages[currencyCode] = allSvgs(fileName);
 }
 
 // Export Images as an object with all currencies required
@@ -67,69 +97,69 @@ const currenciesByCode: IndexByISOCode = {};
 const currenciesByNumber: IndexByISONumber = {};
 
 interface IsoCodeEntry {
-    code: ISOCurrencyCode;
-    isoNumber: ISOCurrencyNumber;
-    precision: string;
-    name: string;
+	code: ISOCurrencyCode;
+	isoNumber: ISOCurrencyNumber;
+	precision: string;
+	name: string;
 }
 
 interface IsoCodeEntryWithImage extends IsoCodeEntry {
-    code: ISOCurrencyCode & CurrencyCode;
-    flag: string;
+	code: ISOCurrencyCode & CurrencyCode;
+	flag: string;
 }
 
 let isoCacheUpdated = false;
 function updateISOCache() {
-    if (isoCacheUpdated) {
-        return;
-    }
+	if (isoCacheUpdated) {
+		return;
+	}
 
-    for (let i = 0; i < ISOCurrencies.length; i++) {
-        const { code, isoNumber } = ISOCurrencies[i];
-        
-        currenciesByCode[code] = i;
-        currenciesByNumber[isoNumber] = i;
-    }
+	for (let i = 0; i < ISOCurrencies.length; i++) {
+		const { code, isoNumber } = ISOCurrencies[i];
 
-    isoCacheUpdated = true;
+		currenciesByCode[code] = i;
+		currenciesByNumber[isoNumber] = i;
+	}
+
+	isoCacheUpdated = true;
 }
 
 function formatIsoLookup(entry: IsoCodeEntry, addImage?: boolean): IsoCodeEntry | IsoCodeEntryWithImage | null {
-    if (!entry) {
-        return null;
-    }
+	if (!entry) {
+		return null;
+	}
 
-    if (addImage !== true) {
-        return entry;
-    }
+	if (addImage !== true) {
+		return entry;
+	}
 
-    if (!isCurrencyCode(entry.code)) {
-        throw new Error(`Cannot get svg for currency code ${entry.code}`);
-    }
+	if (!isCurrencyCode(entry.code)) {
+		throw new Error(`Cannot get svg for currency code ${entry.code}`);
+	}
 
-    return({
-        ...entry,
-        code: entry.code,
-        flag: Images[entry.code]
-    });
+	return({
+		...entry,
+		code: entry.code,
+		flag: Images[entry.code]
+	});
 }
 
 export function getByISOCode(code: ISOCurrencyCode, returnImage: true): IsoCodeEntryWithImage | null;
 export function getByISOCode(code: ISOCurrencyCode, returnImage?: false): IsoCodeEntry | null;
 export function getByISOCode(code: ISOCurrencyCode, returnImage?: boolean): IsoCodeEntry | IsoCodeEntryWithImage | null {
-    updateISOCache();
+	updateISOCache();
 
-    const found = ISOCurrencies[(currenciesByCode as Required<IndexByISOCode>)[code]];
+	const found = ISOCurrencies[(currenciesByCode as Required<IndexByISOCode>)[code]];
 
-    return formatIsoLookup(found, returnImage);
+	return formatIsoLookup(found, returnImage);
 }
 
 export function getByISONumber(isoNumber: ISOCurrencyNumber, returnImage: true): IsoCodeEntryWithImage | null;
 export function getByISONumber(isoNumber: ISOCurrencyNumber, returnImage?: false): IsoCodeEntry | null;
 export function getByISONumber(isoNumber: ISOCurrencyNumber, returnImage?: boolean): IsoCodeEntry | IsoCodeEntryWithImage | null {
-    updateISOCache();
-    
-    const found = ISOCurrencies[(currenciesByNumber as Required<IndexByISONumber>)[isoNumber]]
+	updateISOCache();
 
-    return formatIsoLookup(found, returnImage);
+	const found = ISOCurrencies[(currenciesByNumber as Required<IndexByISONumber>)[isoNumber]]
+
+	return formatIsoLookup(found, returnImage);
 }
